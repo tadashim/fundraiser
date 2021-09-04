8b242bddaab92113a002ee3d00b1d3ae9d9036b5
tadashim <ma.tadashi@gmail.com>
2021-09-04 21:38:44 +0900

    add withdraw function

--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -132,4 +132,60 @@ contract("Fundraiser", accounts => {
       assert.equal(actualEvent, expectedEvent, "events should match");
     });
   });
+
+  describe("withdrawing funds", () => {
+    beforeEach(async () => {
+      await fundraiser.donate(
+        {from: accounts[2], value: web3.utils.toWei('0.1')}
+      );
+    });
+
+    describe("access controls", () => {
+      it("throws an error when called from a non-owner account", async () => {
+        try {
+          await fundraiser.withdraw({from: accounts[3]});
+          assert.fail("withdraw was not restricted to owners");
+        } catch(err) {
+          const expectedError = "Ownable: caller is not the owner";
+          const actualError = err.reason;
+          assert.equal(expectedError, actualError, "should not be permitted");
+        }
+      });
+
+      it("permits the owner to call the function", async () => {
+        try {
+          await fundraiser.withdraw({from: owner});
+          assert(true, "no errors were thrown");
+        } catch(err) {
+          assert.fail("should not have thrown an error");
+        }
+      });
+    });
+
+    it("transfers balance to beneficiary", async () => {
+      const currentContractBalance = await web3.eth.getBalance(fundraiser.address);
+      const currentBeneficiaryBalance = await web3.eth.getBalance(beneficiary);
+
+      await fundraiser.withdraw({from: owner});
+
+      const newContractBalance = await web3.eth.getBalance(fundraiser.address);
+      const newBeneficiaryBalance = await web3.eth.getBalance(beneficiary);
+      const beneficiaryDifference = newBeneficiaryBalance - currentBeneficiaryBalance;
+
+      assert.equal(newContractBalance, 0, "contract should have a 0 balance");
+      assert.equal(
+        beneficiaryDifference,
+        currentContractBalance,
+        "contract should receive all the funds"
+      );
+    });
+
+    it("emits Withdraw event", async () => {
+      const tx = await fundraiser.withdraw({from: owner});
+      const expectedEvent = "Withdraw";
+      const actualEvent = tx.logs[0].event;
+
+      assert.equal(expectedEvent, actualEvent, "events should match");
+    });
+  });
 });
