4f9a387cf9c4e60d7349aedc97f9a04a9d77d31b
tadashim <ma.tadashi@gmail.com>
2021-09-07 23:17:50 +0900

    desplays fundraiser instances

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -5,6 +5,7 @@ import "./Fundraiser.sol";
 
 contract FundraiserFactory {
     Fundraiser[] private _fundraisers;
+    uint256 constant maxLimit = 20;
 
     event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
 
@@ -31,5 +32,24 @@ contract FundraiserFactory {
 
     function fundraisersCount() public view returns(uint256) {
         return _fundraisers.length;
+    }
+
+    function fundraisers(uint256 limit, uint256 offset)
+        public
+        view
+        returns(Fundraiser[] memory coll)
+    {
+        require(offset <= fundraisersCount(), "offset out of bounds");
+
+        uint256 size = fundraisersCount() - offset;
+        size = size < limit ? size : limit;
+        size = size < maxLimit ? size : maxLimit;
+        coll = new Fundraiser[](size);
+
+        for(uint256 i = 0; i < size; i++) {
+            coll[i] = _fundraisers[offset + i];
+        }
+
+        return coll;
     }
 }
